25f43b065cb5366cdc99cd4195090a30b95c695a
todd <todd@wireservers.com>
2024-04-02 20:26:06 -0600

    adding staging yml and removed logs

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -16,7 +16,6 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {}
 
   navigate(section: string) {
-    console.log(section);
     this.navService.navigate(this.route.url, section);
   }
 }
