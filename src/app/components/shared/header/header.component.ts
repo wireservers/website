f693b7f3bbf35f8252ccd54bd778bdfbbd60940a
tclarkston <37628195+tclarkston@users.noreply.github.com>
2024-04-03 16:59:44 -0600

    Adding store pattern and services

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { NavigationService } from '../../services/navigation.service';
-import { Router } from '@angular/router';
+import { Component, OnInit, inject } from '@angular/core';import { Router } from '@angular/router';
+import { NavigationService } from '../../../shared/services/navigation.service';
 
 @Component({
   standalone: true,
